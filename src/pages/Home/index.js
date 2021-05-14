7417ab1f3ab88c583177d4405e1c8a3aceb40229
Roselyn Le <roselynle95@gmail.com>
2021-05-14 15:03:52 +0100

    create login and register button on homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,14 @@ import React from "react";
 const Home = () => {
     return (
         <>
-            <h1>Hello World!</h1>
+            <h1>PlanEat</h1>
+            <p>Plan It, Prep It, Eat It!</p>
+            <a href="http://0.0.0.0:8080/login">
+            <input type="button" value="Login" />
+            </a>
+            <a href="http://0.0.0.0:8080/register">
+            <input type="button" value="Register" />
+            </a>
         </>
     );
 };
