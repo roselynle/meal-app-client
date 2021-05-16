107f2784cfadff54dc1a89a1f3e736a167fe6a41
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-16 19:03:39 +0100

    remove form from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import * as Pages from "./pages";
-import AddRecipeForm from "./components/AddRecipeForm"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import './style.css';
@@ -13,7 +12,7 @@ function App() {
                 <Route exact path="/" component={Pages.Home} />
                 <Route path="/recipes" component={Pages.Recipes} />
             </Switch>
-            <AddRecipeForm/>
+            
         </>
     );
 }
