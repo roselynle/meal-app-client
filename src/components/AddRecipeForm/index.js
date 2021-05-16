ebb1086222714628f0a9991c3e0c60aedb119ecb
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-16 18:53:05 +0100

    add tests for addreceipeform, 100% line coverage

--- src/components/AddRecipeForm/index.js
+++ src/components/AddRecipeForm/index.js
@@ -20,117 +20,109 @@ function AddRecipeForm() {
 
   const onSubmit = (data) => console.log("data", data);
 
-  const dietaryRequirements  = ['Vegan', 'Vegetarian','Pescatarian', 'Gluten-free', 'Diary-free', 'Nut-free']
+  const dietaryRequirements  = ['Vegan', 'Vegetarian','Pescatarian', 'Gluten-free', 'Dairy-free', 'Nut-free']
 
   // Ability to control fields - like onchange
-  const watchRecipeName = watch("recipeName");
-  const watchDescription = watch("recipeDescription");
-  const watchIngredientInput =watch(`ingredients`)
-  const instructions =watch(`Instructions`)
+  // const watchRecipeName = watch("recipeName");
+  // const watchDescription = watch("recipeDescription");
+  // const watchIngredientInput =watch(`ingredients`)
+  // const instructions =watch(`Instructions`)
 
 
 
-  console.log(watchRecipeName);
-  console.log(watchDescription)
-  console.log(watchIngredientInput)
-  console.log(instructions)
+  // console.log(watchRecipeName);
+  // console.log(watchDescription)
+  // console.log(watchIngredientInput)
+  // console.log(instructions)
 
   // The following is useWatch example
   // console.log(useWatch({ name: "test", control }));
 
 
 
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div class="form-group">
-      <label htmlFor="recipeName">Recipe Name:</label>
-      <input id="recipeName" {...register("recipeName")} />
+    <form aria-label="Add a recipe" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-group">
+        <label htmlFor="recipeName">Recipe Name:</label>
+        <input type="text" id="recipeName" {...register("recipeName")} />
       </div>
 
-      <div class="form-group">
-      <label htmlFor="recipeDescription">Description:</label>
-      <input {...register("recipeDescription")} />
+      <div className="form-group">
+        <label htmlFor="recipeDescription">Description:</label>
+        <input type="text" id="recipeDescription" {...register("recipeDescription")} />
       </div>
 
-      <div class="form-group">
-      <label htmlFor="imageUpload">Upload an image:</label>
-      <input type="file" {...register("image-upload")} />
+      <div className="form-group">
+        <label htmlFor="imageUpload">Upload an image:</label>
+        <input type="file" {...register("image-upload")} />
       </div>
 
-      <div  class="form-group">
-      <label htmlFor="">Add your ingredients:</label>
-      <ul>
-        {fields.map((item, index) => {
-          return (
-            <li class="ingredient" key={item.id}>
-          
-           
-                <input placeholder= "2"
-                defaultValue={`${item.amount}`} // make sure to set up defaultValue
-                {...register(`ingredients.${index}.amount`)}
-              />
-               <select defaultValue={`${item.measure}`} // make sure to set up defaultValue
-                {...register(`ingredients.${index}.measure`)}>
-                    <option value="null">-</option>
+      <div  className="form-group">
+        <label htmlFor="">Add your ingredients:</label>
+        <ul>
+          {fields.map((item, index) => {
+            return (
+              <li className="ingredient" key={item.id}>
+                  <input type="text" placeholder= "2"
+                  defaultValue={`${item.amount}`} // make sure to set up defaultValue
+                  {...register(`ingredients.${index}.amount`)}
+                />
+                <select defaultValue={`${item.measure}`} // make sure to set up defaultValue
+                  {...register(`ingredients.${index}.measure`)}>
+                      <option value="null">-</option>
 
-                    <option value="g">kg</option>
-                    <option value="g">g</option>
-                    <option value="L">L</option>
-                    <option value="mL">ml</option>
-                    <option value="tbsp">tbsp</option>
-                    <option value="tsp">tsp</option> 
-                    {/* NEED TO EDIT */}
+                      <option value="g">kg</option>
+                      <option value="g">g</option>
+                      <option value="L">L</option>
+                      <option value="mL">ml</option>
+                      <option value="tbsp">tbsp</option>
+                      <option value="tsp">tsp</option> 
+                      {/* NEED TO EDIT */}
 
-                </select>
-                <input
-              placeholder="eggs"
-                defaultValue={`${item.ingredient}`} // make sure to set up defaultValue
-                {...register(`ingredients.${index}.ingredient`)}
-              />
-
-            
-              <button id="delete-button" type="button" onClick={() => remove(index)}>
-              &#10008;
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-     
-      <section>
-        <button
-          type="button"
-          onClick={() => {
-            append({ amount:"", measure: "",ingredient: "" });
-          }}
-        >
-          Add Ingredient
-        </button>
-       
-
-      </section>
+                  </select>
+                  <input type="text"
+                placeholder="eggs"
+                  defaultValue={`${item.ingredient}`} // make sure to set up defaultValue
+                  {...register(`ingredients.${index}.ingredient`)}
+                />
+                <button aria-role="delete ingredient" id="delete-button" type="button" onClick={() => remove(index)}>
+                &#10008;
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      
+        <section>
+          <button aria-label="add ingredient"
+            type="button"
+            onClick={() => {
+              append({ amount:"", measure: "",ingredient: "" });
+            }}
+          >
+            Add Ingredient
+          </button>
+        </section>
       </div>
 
-          
-      <div class="form-group">
-
-      <label htmlFor="instructions">Instructions:</label>
-      <textarea {...register("Instructions")}></textarea>
+  
+      <div className="form-group">
+        <label htmlFor="instructions">Instructions:</label>
+        <textarea id="instructions" {...register("Instructions")}></textarea>
       </div>
 
 
       <fieldset id="dietary-requirements">
-      <legend>Is your food any of the following?</legend>
-      {dietaryRequirements.map((req, index) => {
-          return (
-            <div class="checkbox">
-            <label htmlFor={`${req}`}>{`${req}`}</label>
-            <input type="checkbox"  id={`${req}`} {...register(`dietary-req.${index}.${req}`)}/>
-            </div>
+        <legend>Is your food any of the following?</legend>
+        {dietaryRequirements.map((req, index) => {
+            return (
+              <div className="checkbox" aria-checked="false" key={index}>
+              <label htmlFor={`${req}`}>{`${req}`}</label>
+              <input type="checkbox"   id={`${req}`} {...register(`dietary-req.${index}.${req}`)}/>
+              </div>
 
-          )})}
-      </fieldset>
+            )})}
+        </fieldset>
 
       <input type="submit" />
     </form>
--- src/components/AddRecipeForm/test.js
+++ src/components/AddRecipeForm/test.js
@@ -1,11 +1,81 @@
 import { screen } from '@testing-library/react';
 import AddRecipeForm from './';
 
-describe('App', () => {
+describe('Form', () => {
 
     beforeEach(() => {
         render(<AddRecipeForm />);
     });
+
+    test("is rendered", () => {
+        let form = screen.getByRole('form')
+        expect(form).toBeInTheDocument();
+        expect(form).toHaveFormValues({
+            recipeName: ''
+           
+          })
+      });
+
+
+    test("has an input for recipe name", () => {
+        let input = screen.getByLabelText('Recipe Name:')
+        expect(input).toBeInTheDocument();
+      });
+
+    test("has an input for the recipe description", () => {
+    let input = screen.getByLabelText('Description:')
+    expect(input).toBeInTheDocument();
+    });
+
+    test("has an input for recipe name", () => {
+        let input = screen.getByLabelText('Instructions:')
+        expect(input).toBeInTheDocument();
+        });
+  
+     
+    test("has 6 checkboxes", () => {
+        let checkboxes = screen.getAllByRole('checkbox')
+        expect(checkboxes).toHaveLength(6)
+        });
+        
+
+    test("ingredient input appears on click of Add Ingredient button", () => {
+        let button = screen.getByText('Add Ingredient')
+        userEvent.click(button)
+        let ingredient = screen.getByRole('listitem')
+        expect(ingredient).toBeInTheDocument()
+        });
+
+    test("added ingredient has a delete ingredient button with an x and a drop down list with 7 options", () => {
+        let button = screen.getByText('Add Ingredient')
+        userEvent.click(button)
+        let deleteButton = screen.getByText('✘')
+        expect(deleteButton).toBeInTheDocument()
+        let options = screen.queryAllByRole('option')
+        expect(options).toHaveLength(7)
+
+        });
+
+    test("can delete an added ingredient on click of x button", () => {
+        let button = screen.getByText('Add Ingredient')
+        userEvent.click(button)
+        let deleteButton = screen.getByText('✘')
+        userEvent.click(deleteButton)
+        let ingredient = screen.queryByRole('listitem')
+        expect(ingredient).not.toBeInTheDocument()
+
+
+        
+
+        });
+                    
+                
+            
+    
+    
+        
+
+
 
 
 })
