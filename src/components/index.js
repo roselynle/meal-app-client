5cbe6f13ce18ee3bd54974e698433f7aca4be011
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-16 19:00:50 +0100

    delete add ingredient component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 // export { default as ComponentName } from './ComponentName' 
 export { default as AddRecipeForm } from './AddRecipeForm'
-export { default as AddIngredient } from './AddIngredient'
 
 
