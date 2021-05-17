18efd5c5203deaca6a32ee5beb9330791a3a8921
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-17 17:07:33 +0100

    attempt to get recipes from store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,13 @@
-store.js
-
-
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { recipeReducer } from './reducers'
+import {recipeReducer} from './reducers'
 
 
 const rootReducer = combineReducers({
     recipeReducer, // key name same as the carefully renamed default export
-    // specific key name instead of the variable name
+   
   })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
