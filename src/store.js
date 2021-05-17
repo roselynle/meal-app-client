afbb83e7279e8405cbf6006e51cab8ebe7ce932e
Roselyn Le <roselynle95@gmail.com>
2021-05-17 23:27:16 +0100

    add user reducer to store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,12 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import {recipeReducer} from './reducers'
+import {userReducer, recipeReducer} from './reducers'
 
 
 const rootReducer = combineReducers({
     recipeReducer, // key name same as the carefully renamed default export
-   
+    user: userReducer,
   })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
