2bc75fcf9be00cc7a1ab2705a5c70f64937a0faa
Roselyn Le <roselynle95@gmail.com>
2021-05-17 10:47:59 +0100

    test meal cards

--- src/components/MealCards/index.js
+++ src/components/MealCards/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 
-const days = [ {day:"1"},  {day:"2"},  {day:"3"},  {day:"4"},  {day:"5"},  {day:"6"},  {day:"7"}]
-
+const days = [
+    { id: 1, day: "1" },
+    { id: 2, day: "2" },
+    { id: 3, day: "3" },
+    { id: 4, day: "4" },
+    { id: 5, day: "5" },
+    { id: 6, day: "6" },
+    { id: 7, day: "7" },
+];
 
 const MealCards = () => {
-    return days.map(d =>
-        <div className="meal-card">
+    return days.map((d) => (
+        <div key={d.id} className="meal-card">
             <div className="meal-card-body">
                 <h3 className="meal-card-title">Day {d.day}</h3>
                 <p>Meal</p>
             </div>
         </div>
-    );
+    ));
 };
 
 export default MealCards;
---
+++ src/components/MealCards/test.js
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import MealCards from '.';
+
+describe('Meal Cards', () => {
+    beforeEach(() => {
+        render(<MealCards />, { wrapper: MemoryRouter });
+    });
+
+    describe('meal cards', () => {
+        test('it renders 7 meal cards for each day', () => {
+            expect(screen.getAllByRole('heading')).toHaveLength(7)
+        })
+    })
+})
