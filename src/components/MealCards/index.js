15606be3daa5e66c7b2a16cd24b1b481266c5c86
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-17 16:30:59 +0100

    add actions

--- src/components/MealCards/index.js
+++ src/components/MealCards/index.js
@@ -9,7 +9,6 @@ const MealCards = () => {
             <div className="meal-card-body">
                 <h3 className="meal-card-title">Day {d.day}</h3>
                 <p>Meal</p>
-                <h1> hello</h1>
             </div>
         </div>
     );
