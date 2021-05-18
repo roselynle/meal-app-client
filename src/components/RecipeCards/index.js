89aa9a02ec846ca250c00ada64f566dfad3168ed
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-18 20:02:25 +0100

    edit recipe card to show uploaded image

--- src/components/RecipeCards/index.js
+++ src/components/RecipeCards/index.js
@@ -16,13 +16,13 @@ const RecipeCards = (props) => {
     const  recipes  = useSelector(state => state.recipes)
     const loading = useSelector(state => state.loading)
 
-    console.log(typeof(props.recipe.id))
+    console.log(props.recipe.img_url)
 
     const url = `recipes/${props.recipe.id}`
 
     return (
         <div className="recipe-card">
-            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEeQdNORa8Fpm-w-iSPYlURM87gv_DH-MyjQ&usqp=CAU"} className="card-img-top" alt="Recipe Image"></img>
+            <img src={props.recipe.img_url} className="card-img-top" alt="Recipe Image"></img>
             <div className="recipe-card-body">
                 <h3 className="recipe-card-title">{props.recipe.recipe_name}</h3>
                 <p>{props.recipe.recipe_description}</p>
