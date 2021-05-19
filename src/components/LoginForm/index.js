dca2a6315f7dc9780d2eaec2620542daf3afaee6
Roselyn Le <roselynle95@gmail.com>
2021-05-19 10:21:03 +0100

    refactor login

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import "../../App.css";
-import { useDispatch } from "react-redux";
 import { useHistory } from 'react-router-dom'
 import { loginUser } from "../../actions"
 
-const LoginForm = () => {
+const LoginForm = ({login}) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const dispatch = useDispatch();
     const history = useHistory()
 
     const handleUsername = (e) => {
@@ -28,35 +26,43 @@ const LoginForm = () => {
     // };
 
 
-async function handleSubmit(e){
-    e.preventDefault();
-    const loginData = { username, password }
-    console.log(loginData)
-    try {
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(loginData)
-        }
-        const r = await fetch(`http://localhost:5000/login`, options)
-        console.log(r)
-        const data = await r.json()
-        console.log(data)
-        if (data.err){ throw Error(data.err); }
-        login(data);
-    } catch (err) {
-        console.warn(`Error: ${err}`);
-    }
-}
+// async function handleSubmit(e){
+//     e.preventDefault();
+//     const loginData = { username, password }
+//     console.log(loginData)
+//     try {
+//         const options = {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify(loginData)
+//         }
+//         const r = await fetch(`http://localhost:5000/login`, options)
+//         console.log(r)
+//         const data = await r.json()
+//         console.log(data)
+//         if (data.err){ throw Error(data.err); }
+//         login(data);
+//     } catch (err) {
+//         console.warn(`Error: ${err}`);
+//     }
+// }
 
-function login(data){
-    console.log(data)
-    sessionStorage.setItem('username', data);
-    history.push("/meals")
-}
+// function login(data){
+//     console.log(data)
+//     sessionStorage.setItem('username', data);
+//     history.push("/meals")
+// }
+
+console.log(login)
+
+loginUser = e => {
+        e.preventDefault();
+        login(username, password)
+        console.log("hello")
+    }
 
     return (
-            <form onSubmit={handleSubmit} id="login-form">
+            <form onSubmit={loginUser} id="login-form">
                 <div className="login-input">
                     <label htmlFor="username">Username:</label>
                     <input type="text" name="username" onChange={handleUsername}/>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,12 +3,13 @@ import { LoginForm } from "../../components";
 import { NavLink } from "react-router-dom";
 import "../../App.css";
 
-const Login = () => {
+const Login = ({login}) => {
+    console.log(login)
     return (
         <>
         <div className="login-container">
             <h1>Login</h1>
-            <LoginForm/>
+            <LoginForm login={login}/>
             <NavLink to="/register" >
             <a>Don't have an account yet?</a>
             </NavLink>
