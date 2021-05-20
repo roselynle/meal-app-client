d321bb7424ee21ebfd5b70367517a1b33b07bebc
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-20 18:38:49 +0100

    clean up recipe page

--- src/pages/RecipePage/index.js
+++ src/pages/RecipePage/index.js
@@ -1,35 +1,24 @@
 import React, { useEffect } from "react"
-import { AddtoCartBtn, SaveRecipeButton, NavBar, BackButton } from '../../components'
+import { SaveRecipeButton, NavBar, BackButton } from '../../components'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from "react-router"
 import { fetchRecipeDetails } from "../../actions"
-import { useHistory } from 'react-router-dom';
-
 import "./style.css"
-
-
 
 const RecipePage = () => {
     const params = useParams()
     const id = params.id
-    console.log(id)
+    const recipe = useSelector(state => state.singleRecipeReducer.recipe)
+    const error = useSelector(state => state.singleRecipeReducer.error)
+    const loading = useSelector(state => state.singleRecipeReducer.loading)
+    const ingredients = recipe.ingredients
+    const diet_reqs = recipe.diet_req
 
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(fetchRecipeDetails(id))
     }, [])
 
-    const recipe = useSelector(state => state.singleRecipeReducer.recipe)
-    const error = useSelector(state => state.singleRecipeReducer.error)
-    const loading = useSelector(state => state.singleRecipeReducer.loading)
-
-
-    
-    const ingredients = recipe.ingredients
-    console.log(ingredients)
-    const diet_reqs = recipe.diet_req
-
-    console.log(diet_reqs)
 
     if (error) {
         return (<h1>Oops... this recipe does not exist</h1>)
@@ -40,13 +29,11 @@ const RecipePage = () => {
             <>
                 <NavBar />
                 <BackButton/>
-
-
                 {
                     loading ?
                         <h2>Loading . . .</h2>
                         :
-                        <>
+          
                         <div className="recipeInfo">
                             <div className="headers">
                                 <h1>{recipe.title} </h1>
@@ -58,12 +45,11 @@ const RecipePage = () => {
 
                             <div className="instructions">
                                 <p>{recipe.instructions} </p>
-                                {/* <ul> { diet_req && diet_req.map(item => <li>{item}</li>)}</ul> */}
                                 <SaveRecipeButton id={id} />
                             </div>
 
                         </div>
-                        </>
+              
                 }
             </>
 
