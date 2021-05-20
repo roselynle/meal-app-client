577cf90f0fd5f5df52906c9dc8533b9f43c9742f
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-20 12:11:56 +0100

    tidy up recipe card and change text to see recipe

--- src/components/RecipeCards/index.js
+++ src/components/RecipeCards/index.js
@@ -5,21 +5,6 @@ import "./style.css"
 import {SaveRecipeButton} from '..'
 
 const RecipeCards = (props) => {
-    // const history = useHistory()
-
-    // const handleclick = (id) => {
-    //     history.push(`/recipes/${id}`)
-    //     //history.push keeps react state whereas redirect clears on refresh
-    // }
-
-    // const dispatch = useDispatch()
-    // const  recipes  = useSelector(state => state.recipes)
-    // const loading = useSelector(state => state.loading)
-
-    // console.log(props.recipe.id)
-
- 
-    //const url = `recipes/${props.recipe.id}`
 
     return (
         <div className="recipe-card">
@@ -29,28 +14,14 @@ const RecipeCards = (props) => {
                 <p>{props.recipe.description}</p>
                 <ul> { props.recipe.diet_req && props.recipe.diet_req.map((item, index) => <li key={index}>{item}</li>)}</ul>
 
-                {/* <Redirect to={`recipes/${props.recipe.id}`}> */}
-                {/* <a
-                    href={`http://0.0.0.0:8080/recipes/${props.recipe.id}`} 
-                    target="blank"
-                    className="recipe-link"
-                >
-                    See recipe here!
-                </a> */}
-                
 
-                <Link to={`/recipes/${props.recipe._id}`}>See more</Link>
-                {/* <Link to={`/recipes/${props.recipe.id}`}>See more</Link> */}
-                 {/* <button onClick={() => handleclick(props.recipe.id)}>
-                    See recipe here!
-                </button>  */}
+                <Link to={`/recipes/${props.recipe._id}`}>See recipe</Link>
+           
                 {props.showFavBtn ? <SaveRecipeButton id={props.recipe._id}/>: <button>x</button>
                 }
                 
    
    
-                {/* link above to be updated */}
-                {/* Create a save button and import here */}
             </div>
         </div>
     );
